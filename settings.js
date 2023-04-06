87db18b8490ab3bbbcc1532581c15e42c4a5de12
jiunxing10 <129085147+jiunxing10@users.noreply.github.com>
2023-04-06 07:15:58 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
 global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.MONGO_DB_URI = "mongodb+srv://ApiBotWA:1z7s7hmUbgajvdLp@cluster0.4psfxhy.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "dioaldanioo@gmail.com" //email
+global.email_password = "okrwxkdhxhvyyzox" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
